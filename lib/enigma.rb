135327a08332551db2450ffccfdefe479bb8e86e
Kaelin Sleevi <kaelin.sleevi@outlook.com>
2022-08-07 15:22:57 -0600

    feat: start to encrypt method in enigma class

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,10 +1,10 @@
 class Enigma
 
-  # def encrypt
-  #
-  # end
-  #
-  # def decrypt
-  #
-  # end
+  def encrypt(message, key, date)
+    hash_output = {
+      message: message,
+      key: key,
+      date: date
+    }
+  end
 end
