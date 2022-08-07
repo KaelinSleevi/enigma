271ee5252a6a560fecad93d04c6e922be56ffaf9
Kaelin Sleevi <kaelin.sleevi@outlook.com>
2022-08-07 12:45:21 -0600

    delete files that are not needed

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,7 +1,15 @@
 class Enigma
 
-  def encrypt
+  def encrypt(message, keys = key, dates = date)
 
+  end
+
+  def date
+    Time.now.strftime("%d%m%y")
+  end
+
+  def key
+    rand(100000).to_s.rjust(5, "0")
   end
 
 end
