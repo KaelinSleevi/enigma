63bb39869dea6acc4e399d125093cf9f84bf6694
Kaelin Sleevi <kaelin.sleevi@outlook.com>
2022-08-08 16:14:32 -0600

    fixed calculables and added rotation methods for encrypt

--- lib/calculables.rb
+++ lib/calculables.rb
@@ -9,19 +9,41 @@ module Calculables
   end
 
   def key_calculator
-    @a = @key[0..1].to_i,
-    @b = @key[1..2].to_i,
-    @c = @key[2..3].to_i,
-    @d = @key[3..4].to_i
+    {
+    A: @key[0..1].to_i,
+    B: @key[1..2].to_i,
+    C: @key[2..3].to_i,
+    D: @key[3..4].to_i
+  }
   end
 
-  def offset_shift_calculator
+  def offset_calculator
     last_four = (@date.to_i**2).to_s[-4..-1]
-    shifts = {
-    a_shift: (last_four[0].to_i + @a),
-    b_shift: (last_four[1].to_i + @b),
-    c_shift: (last_four[2].to_i + @c),
-    d_shift: (last_four[3].to_i + @d)
+    {
+    A: last_four[0].to_i,
+    B: last_four[1].to_i,
+    C: last_four[2].to_i,
+    D: last_four[3].to_i
   }
+  end
+
+  def shifts
+    key_calculator.merge!(offset_calculator){ |key, key_value, offset_value| key_value + offset_value }
+  end
+
+  def a_rotation
+    Hash[@alphabet.zip(@alphabet.rotate(offset_calculator[:A]))]
+  end
+
+  def b_rotation
+    Hash[@alphabet.zip(@alphabet.rotate(offset_calculator[:B]))]
+  end
+
+  def c_rotation
+    Hash[@alphabet.zip(@alphabet.rotate(offset_calculator[:C]))]
+  end
+
+  def d_rotation
+    Hash[@alphabet.zip(@alphabet.rotate(offset_calculator[:C]))]
   end
 end
--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -10,6 +10,16 @@ RSpec.describe Enigma do
     expect(@enigma).to be_an(Enigma)
   end
 
+  it 'has an alphabet' do
+    expect(@enigma.alphabet).to eq(["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "])
+  end
+
+  it 'has messages, keys, or dates generated' do
+    expect(@enigma.message).to eq('')
+    expect(@enigma.key).to eq('')
+    expect(@enigma.date).to eq('')
+  end
+
   it 'can encrypt messages' do
     expected = {
       encryption: "keder ohulw",
