7c9b99688d26150be8b58b2ee83d38fc0a1cc915
Kaelin Sleevi <kaelin.sleevi@outlook.com>
2022-08-08 18:22:12 -0600

    update enigma encrypt method and calculables

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -2,35 +2,20 @@ require './lib/calculables'
 
 class Enigma
   include Calculables
-  attr_reader :message,
-              :key,
-              :date,
-              :alphabet
+  attr_reader :alphabet
 
   def initialize
-    @message = ''
-    @key = ''
-    @date = ''
     @alphabet = ('a'..'z').to_a << ' '
   end
 
   def encrypt(message, key, date)
-    @message = 'hello world'
-    @key = generate_keys
-    @date = generate_date
+    @message = message.downcase
+    @key = key
+    @date = date
 
     message_array = @message.downcase.chars
-    encrypted_message = message_array.each_with_index.map do |character, index|
-      if (index) % 4 == 0 then character = a_rotation[character]
-      elsif (index) % 4 == 1 then character = b_rotation[character]
-      elsif (index) % 4 == 2 then character = c_rotation[character]
-      elsif (index) % 4 == 3 then character = d_rotation[character]
-      end
-    end
-    encrypted_hash = {:encryption => encrypted_message.join, :key => @key, :date => @date}
   end
 end
-
 #
 #take the key and the date to find the offset_shift_calculator
 #iterate through the message and shift through each character
