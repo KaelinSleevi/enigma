50e2c7e19f9b921f6255133ede4a53a149199f7b
Kaelin Sleevi <kaelin.sleevi@outlook.com>
2022-08-08 22:03:56 -0600

    add completed base level decrypt method

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -20,6 +20,25 @@ class Enigma
    encrypted = {encryption: encrypted_message.join, key: @key, date: @date}
  end
 
+ def decrypt(message, key, date)
+   @message = message.downcase
+   @key = key
+   @date = date
+
+   message_array = @message.chars
+   decrypted_message = message_array.each_with_index.map do |char, index|
+     if index % 4 == 0
+       char = a_key_rotation[char].reverse
+     elsif index % 4 == 1
+       char = b_key_rotation[char].reverse
+     elsif index % 4 == 2
+       char = c_key_rotation[char].reverse
+     elsif index % 4 == 3
+       char = d_key_rotation[char].reverse
+     end
+  end
+  decrypted = {decryption: decrypted_message.join, key: @key, date: @date}
+ end
     # message_index_arr = message_array.each_with_index.to_a
     # alphabet_set_index_arr = alphabet_set.each_with_index.to_a
 
@@ -61,11 +80,4 @@ class Enigma
     Hash[alphabet_set.zip(alphabet_set.rotate(key_shifts[:D]))]
   end
 
-  # def decrypt(message, key, date)
-  #   output = {
-  #     encryption: message,
-  #     key: key,
-  #     date: date
-  #   }
-  # end
 end
--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -20,12 +20,12 @@ RSpec.describe Enigma do
     expect(@enigma.encrypt("hello world", "02715", "040895")).to eq(expected)
   end
 
-  xit 'can decrypt messages' do
-    #   {
-    #     decryption: "hello world",
-    #     key: "02715",
-    #     date: "040895"
-    #   }
+  it 'can decrypt messages' do
+      {
+        decryption: "hello world",
+        key: "02715",
+        date: "040895"
+      }
 
     expect(@enigma.decrypt("keder ohulw", "02715", "040895")).to be_a(Hash)
   end
