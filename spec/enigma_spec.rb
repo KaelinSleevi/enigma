ca7843528af777a809d7c6dab57d368920c9c8ed
Kaelin Sleevi <kaelin.sleevi@outlook.com>
2022-08-09 11:45:01 -0600

    finished encrypt and decrypt methods with stubs

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -10,6 +10,19 @@ RSpec.describe Enigma do
     expect(@enigma).to be_an(Enigma)
   end
 
+  it 'can have an alphabet set' do
+    expect(@enigma.alphabet_set.length).to eq(27)
+    expect(@enigma.alphabet_set).to eq(["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "])
+  end
+
+  it 'can generate a random key' do
+    expect(@enigma.key_generator).to be_a(String)
+  end
+
+  it 'can generate a random date' do
+    expect(@enigma.date_generator).to be_a(String)
+  end
+
   it 'can encrypt messages' do
     expected = {
           encryption: "keder ohulw",
@@ -21,11 +34,26 @@ RSpec.describe Enigma do
   end
 
   it 'can encrypt when no date is given' do
-    expect(@enigma.encrypt("hello world", "02715")).to be_a(Hash)
+    expected = {
+          encryption: "keder ohulw",
+          key: "02715",
+          date: "040895"
+        }
+
+    allow_any_instance_of(Enigma).to receive(:date_generator).and_return("040895")
+    expect(@enigma.encrypt("hello world", "02715")).to eq(expected)
   end
 
   it 'can encrypt without a key or date' do
-    expect(@enigma.encrypt("hello world")).to be_a(Hash)
+    expected = {
+          encryption: "keder ohulw",
+          key: "02715",
+          date: "040895"
+        }
+
+    allow_any_instance_of(Enigma).to receive(:date_generator).and_return("040895")
+    allow_any_instance_of(Enigma).to receive(:key_generator).and_return("02715")
+    expect(@enigma.encrypt("hello world")).to eq(expected)
   end
 
   it 'can decrypt messages' do
@@ -38,7 +66,15 @@ RSpec.describe Enigma do
     expect(@enigma.decrypt("keder ohulw", "02715", "040895")).to eq(expected)
   end
 
-  xit 'can decrypt based on encryption key' do
-    expect(@enigma.decrypt(decrypt(encrypted[:encryption], "02715"))).to be_a(Hash)
+  it 'can decrypt based on encryption key' do
+      expected = {
+          decryption: "hello world",
+          key: "02715",
+          date: "040895"
+        }
+
+    
+    allow_any_instance_of(Enigma).to receive(:date_generator).and_return("040895")
+    expect(@enigma.decrypt(encrypted[:encryption], "02715")).to eq(expected)
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,4 @@
 require 'simplecov'
 SimpleCov.start
 
-# Previous content of test helper now starts here
 require './lib/enigma'
